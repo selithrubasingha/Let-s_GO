f5f6026bdb4d1931e3fec488a4e487ef419ef6d7
Selith <selithrubasingha@gmail.com>
2026-07-24 23:30:15 +0530

    tour of go : initialized variables using var

---
+++ tour_of_go/initialized_variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+var l,m,n int = 341, 33 , 2
+
+func main9() {
+	var c, python, java = true, false, "no!"
+	var awesome , phrase , size = true , "I love Pancakes" , 65
+	fmt.Println(i, j, c, python, java)
+	fmt.Println(l,m,n)
+	fmt.Println(awesome , phrase , size)
+}
