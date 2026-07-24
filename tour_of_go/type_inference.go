202a594c93456649680f91254c6ac00f52fd227e
Selith <selithrubasingha@gmail.com>
2026-07-25 00:20:54 +0530

    tour of go : type inference

---
+++ tour_of_go/type_inference.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main13() {
+	v := 42 // change me!
+	w := 67.321
+	x := 12 +43i
+	
+	item1 := v
+	
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("w is of type %T\n", w)
+
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("item1 is of type %T\n", item1)
+
+
+}
