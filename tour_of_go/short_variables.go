a90ea0d444801e42afc206637865222976e14360
Selith <selithrubasingha@gmail.com>
2026-07-24 23:36:35 +0530

    tour of go : short variable declaration

---
+++ tour_of_go/short_variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	
+	c, python, java := true, false, "no!"
+	
+	pancakes , alive , yelling := 34 , true , "No ! don't eat me ! "
+
+	fmt.Println(i, j, k, c, python, java)
+	fmt.Println(pancakes , alive , yelling)
+}
