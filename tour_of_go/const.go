99d753f3c9f5e44441128e1cfda020bdfe91d174
Selith <selithrubasingha@gmail.com>
2026-07-25 00:22:32 +0530

    tour of go : const keyword

---
+++ tour_of_go/const.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+// const cannot be declared with := syntax !!
+
+func main14() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
