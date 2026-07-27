563d1b2fd7c6609c2e63c2019ee566f261857da7
Selith <selithrubasingha@gmail.com>
2026-07-27 23:38:11 +0530

    prject euler : 1

---
+++ project_euler/01/01.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World")
+
+	sum := 0
+
+	for i:=0;i<1000;i++{
+		if i%3==0 || i%5==0 {
+			sum += i
+		}
+
+	}
+
+	fmt.Println(sum)
+
+
+}
